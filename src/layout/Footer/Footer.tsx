888f2e75e18fc3963a913f0dd8ee84cbe191638a
ridoy <mdridoyhossain247@gmail.com>
2023-09-13 00:08:30 +0600

    footer section added

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,10 +1,31 @@
+import { Box, Container, Typography, styled } from "@mui/material"
 import React from "react"
-const Footer = () => {
+
+const Footer = ({ footerHight }) => {
+
+
   return (
-    <div>
-      Footer
-    </div>
+    <Box ref={footerHight} borderTop={'1px solid #413333'}>
+      <Container>
+        <CopyRightTtitle>
+          Copyright 2022 ©Ninja. All rights reserved. Powered by Ninja.
+        </CopyRightTtitle>
+      </Container>
+    </Box>
   )
 }
 
 export default Footer
+
+const CopyRightTtitle = styled(Typography)(({ theme }) => ({
+  fontSize: '18px',
+  color: '#413333',
+  textAlign: 'center',
+  backgroundColor: '#fff',
+  padding: '8px 0',
+  lineHeight: 1.5,
+  [theme.breakpoints.only('xs')]: {
+    fontSize: '15px',
+  }
+}))
+
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,13 +1,29 @@
-import React from "react"
+import { Box } from "@mui/material"
+import React, { useEffect, useRef, useState } from "react"
 import Footer from "./Footer/Footer"
 import Header from "./Header/Header"
+
 const Layout = ({ children }: any) => {
+
+  const [headerHeight1, setHeaderHeight1] = useState(null)
+  const [footerHeight1, setfooterHeight1] = useState(null)
+  const headerHight = useRef();
+  const footerHight = useRef();
+  useEffect(() => {
+    setHeaderHeight1(headerHight.current.clientHeight);
+    setfooterHeight1(footerHight.current.clientHeight);
+
+  }, [])
+
+  const totalHeight = headerHeight1 + footerHeight1
 
   return (
     <>
-      <Header />
-      {children}
-      <Footer />
+      <Header headerHight={headerHight} />
+      <Box minHeight={`calc(100vh - ${totalHeight}px )`}>
+        {children}
+      </Box>
+      <Footer footerHight={footerHight} />
     </>
   )
 }
